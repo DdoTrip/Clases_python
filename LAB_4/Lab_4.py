61fce444c7a68b94fc5f940019285e3a224471f5
Sergio <sergicarrillo96@gmail.com>
2024-05-23 12:07:07 +0200

    Lab_4 terminado

---
+++ LAB_4/Lab_4.py
@@ -0,0 +1,44 @@
+# Definición de una lista
+
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+# Acceso a una lista por posición
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+# Modificación de los valores de una lista
+
+myFruitList[2] = "orange"
+print(myFruitList)
+
+# Deficion de una tupla
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+# Acceso a una tupla por posicion
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+# Definicion de un diccionario
+
+myFavoriteFruitDictionary = {
+    "Akua" : "apple",
+    "Sasnvi" : "banana",
+    "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+# Acceso al diccionario por nombre
+
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Sasnvi"])
+print(myFavoriteFruitDictionary["Paulo"])
